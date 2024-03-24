bf1e3626bc8a3fada9cb546ccf0edd3cfd3a7dd3
Mina Tahmasebi <mina_t_b@outlook.com>
2024-03-24 15:40:44 +0100

    Now includes bastion and reverse proxy in arm and provision.sh

--- cloudinitdotnet.sh
+++ cloudinitdotnet.sh
@@ -11,3 +11,23 @@ rm packages-microsoft-prod.deb
 # Update packages
 apt update
 apt-get install -y aspnetcore-runtime-8.0
+cat << 'EOF' > /etc/systemd/system/InlmnngUpgft2.service
+[Unit]
+Description=My very first ASP.NET Core applications running on Ubuntu
+
+[Service]
+WorkingDirectory=/opt/InlmnngUpgft2
+ExecStart=/usr/bin/dotnet /opt/InlmnngUpgft2/InlmnngUpgft2.dll
+Restart=always
+# Restart service after 10 seconds if the dotnet service crashes:
+RestartSec=10
+KillSignal=SIGINT
+SyslogIdentifier=dotnet-example
+User=www-data
+Environment=ASPNETCORE_ENVIRONMENT=Development
+Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false
+Environment=ASPNETCORE_URLS="http://*:5000"
+
+[Install]
+WantedBy=multi-user.target
+EOF
--- provision.sh
+++ provision.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-rg_name=myResourceGroup1
+rg_name=myResourceGroup8
+token=ARW2HX2NVOZQDABYASIFCO3GABEBI
 
 az group create --location swedencentral --name $rg_name
-az deployment group create --resource-group $rg_name --template-file arm.json --parameters AdminUser=azureuser sshRSAPublicKey="$(cat ~/.ssh/id_rsa.pub)" customDataAppServer=@cloudinitdotnet.sh
-az vm show --resource-group $rg_name --name vmappserver --show-details --query [publicIps] --output tsv
+az deployment group create --resource-group $rg_name --template-file arm.json \
+    --parameters AdminUser=azureuser sshRSAPublicKey="$(cat ~/.ssh/id_rsa.pub)" customDataAppServer=@cloudinitdotnet.sh customDataRevProxies=@cloudinitrevproxy.sh 
+public_ip=$(az vm show --resource-group $rg_name --name vmbastion --show-details --query [publicIps] --output tsv)
+
+eval $(ssh-agent)
+
+ssh-add ~/.ssh/id_rsa
+
+ssh -o StrictHostKeyChecking=no -A -N -L 2222:10.0.0.10:22 azureuser@$public_ip &
+
+ssh -o StrictHostKeyChecking=no -p 2222 azureuser@localhost "mkdir actions-runner && cd actions-runner && \
+    curl -o actions-runner-linux-x64-2.314.1.tar.gz \
+    -L https://github.com/actions/runner/releases/download/v2.314.1/actions-runner-linux-x64-2.314.1.tar.gz && \
+    echo \"6c726a118bbe02cd32e222f890e1e476567bf299353a96886ba75b423c1137b5  actions-runner-linux-x64-2.314.1.tar.gz\" | \
+    shasum -a 256 -c && tar xzf ./actions-runner-linux-x64-2.314.1.tar.gz &&  \
+    ./config.sh --unattended --url https://github.com/mina-mtb/InlmnngUpgft2 --token $token && sudo ./svc.sh install azureuser && sudo ./svc.sh start"
